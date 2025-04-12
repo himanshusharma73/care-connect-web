e9af1c566b74534d1e11476d0e2994bec2bcb33a
Himanshu <Himanshu@gmail.com>
2025-04-12 16:55:43 +0530

    commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -7,7 +8,18 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  constructor(private authService: AuthService) {}
+  notifications: number = 3; // Example notification count (can be dynamic from a service)
+  isSidebarOpen: boolean = true;
+  isMobile: boolean = window.innerWidth < 768;
 
- 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
